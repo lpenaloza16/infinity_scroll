558c6be7af7ee30670e14078ae7eea9b68e8ae54
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-04-13 08:49:03 -0700

    Add photo functionality from API
    This commit adds the ability to retrieve and display photos from an external API. The code uses the API's endpoint to fetch the photos and then displays them on the webpage. The implementation includes handling errors in case the API is down or unavailable. Additionally, the code is optimized to ensure efficient and fast retrieval of photos, while minimizing potential security vulnerabilities. Overall, this functionality enhances the user experience by providing access to a wider range of photos without relying solely on locally stored images.

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photoArray = [];
+
 //Unsplash API
 const count = 10;
-const apiKey = `u7d4w59AkyEWY3y_fS5gxesakg2w6JRdzwYnmwL8hMQ`;
+const apiKey = `Jqb8XX2NExXP5f-H9lyRhhCfNJrtN_HeNV8ca95TGpk`;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+//Helper Functions
+function setAttribute(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
 // Get photos from Unsplash API
+function displayPhotos() {
+  photoArray.forEach((photo) => {
+    const item = document.createElement("a");
+
+    setAttribute(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // create <img> for photo
+    const img = document.createElement("img");
+    // img.setAttribute("src", photo.urls.regular);
+    // img.setAttribute("alt", photo.alt_description);
+    // img.setAttribute("title", photo.alt_description);
+
+    setAttribute(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Put <img> inside <a> then put both inside image_container element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+// Create Elements For Links and Photos
 
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data);
+    photoArray = await response.json();
+    displayPhotos();
   } catch (error) {
     //catch error
     console.log(`fix it`);
